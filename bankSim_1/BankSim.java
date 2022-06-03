8d3a80000682676cbfba07a133a26b31b920196b
Fabio Felipe Gutierrez <1560@holbertonschool.com>
2022-06-02 22:21:05 -0500

    Add: adding main class

---
+++ bankSim_1/BankSim.java
@@ -0,0 +1,15 @@
+package bankSim;
+
+import bankSim.Account;
+
+public class BankSim {
+    public static void main(String[] args) {
+		
+		
+		Account beyonce = new Account("Beyonce", "B00002");
+		beyonce.showMenu();
+		
+		
+	}
+
+}
